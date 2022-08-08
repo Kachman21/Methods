4d24f93d4cbfc8344f38ce6ce1041ae39258dd1c
Kachman21 <Kachman21onyedikachiokafor21@gmail.com>
2022-08-09 00:02:52 +0100

    I made changes to my mehod repository

---
+++ array-methods.js
@@ -0,0 +1,21 @@
+// flat-map-()
+const arr1 = [1, 2, [3], [4, 5], 6, []];
+
+const flattened = arr1.flatMap(num => num);
+
+console.log(flattened);
+
+// concat()
+const array1 = ['a', 'b', 'c'];
+const array2 = ['d', 'e', 'f'];
+const array3 = array1.concat(array2);
+
+console.log(array3);
+
+// some()
+const array = [1, 2, 3, 4, 5];
+
+// checks whether an element is even
+const even = (element) => element % 2 === 0;
+
+console.log(array.some(even));
